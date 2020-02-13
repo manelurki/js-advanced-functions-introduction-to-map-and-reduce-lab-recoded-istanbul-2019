e7a6da2044a04e0500c36221c5e7c584071b21d1
manel turki <menelturki@gmail.com>
2020-02-13 13:18:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-mapToNegativize(sourceArray)
-{
-  let arr=[];
-  
-   for (let i = 0; i < sourceArray.length; i++ ) {
-  arr[i]=sourceArray[i]*(-1);
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i])
   }
-  return arr;
+  return r
 }
-  
-  
+
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i])
+  }
+  return r
+}
+
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i])
+  }
+  return r
+}
+
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i])
+  }
+  return r
+}
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
